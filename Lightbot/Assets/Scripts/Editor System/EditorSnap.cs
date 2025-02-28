4056b618e82e13b179795e632b4db8f356440cb7
SaraBre <Sarabre26@gmail.com>
2025-02-28 15:33:54 +0330

    level1

--- Lightbot/Assets/Scripts/Editor System/EditorSnap.cs
+++ Lightbot/Assets/Scripts/Editor System/EditorSnap.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class EditorSnap : MonoBehaviour
 {
-    [SerializeField] [Range(0f, 20f)] int gradeSize;
+    [SerializeField] [Range(0f, 20f)] float gradeSize;
 
     private void Update()
     {
         Vector3 snap;
         snap.x = Mathf.RoundToInt(transform.position.x / gradeSize) * gradeSize;
-        snap.z = Mathf.RoundToInt(transform.position.z / gradeSize) * gradeSize;
+       // snap.z = Mathf.RoundToInt(transform.position.z / gradeSize) * gradeSize;
         snap.y = Mathf.RoundToInt(transform.position.y / gradeSize) * gradeSize;
         transform.position = snap;
     }
